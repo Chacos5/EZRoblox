dec9ec18afbe73639b22cfcb96e8962e3e588d6a
Chacos5 <75866891+Chacos5@users.noreply.github.com>
2023-07-07 21:13:22 -0700

    Add main code

---
+++ EZRoblox.ps1
@@ -0,0 +1,69 @@
+﻿<#  EZRoblox
+
+    Copyright (C) 2023 Chacos5
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.#>
+Write-Host -ForegroundColor Green "
+  ______ ___________       _     _           
+ |  ____|___  /  __ \     | |   | |          
+ | |__     / /| |__) |___ | |__ | | _____  __
+ |  __|   / / |  _  // _ \| '_ \| |/ _ \ \/ /
+ | |____ / /__| | \ \ (_) | |_) | | (_) >  < 
+ |______/_____|_|  \_\___/|_.__/|_|\___/_/\_\
+                                             
+                                             
+"
+                                                                           
+
+$resourceDir = Get-Location # Define what the root directory of the script is to allow access to the image files
+Write-Host -ForegroundColor Green "Resource directory is: "$resourceDir 
+
+Write-Host -ForegroundColor Green "Running Roblox FPS unlocker downloader"
+
+Invoke-Expression -Command .\data\FPSUnlockerDownload.ps1 # Before running anything run the FPS unlocker script
+
+
+
+# Start of crosshair modification
+Write-Host -ForegroundColor Green "Starting crosshair modification"
+
+Write-Host -ForegroundColor Green "Moving to Roblox Directory"
+cd "$Env:localappdata\Roblox\Versions" # Move to the roblox folder
+
+Write-Host -ForegroundColor Green "Newest folder is:" $(Get-ChildItem | Where { $_.PSIsContainer } | Sort CreationTime -Descending | Select -First 1)
+
+Get-ChildItem | Where { $_.PSIsContainer } | Sort CreationTime -Descending | Select -First 1 | cd  # Find the newest folder and move into it with cd
+
+
+Write-Host -ForegroundColor Green "Moving to keyboardmouse textures directory"
+cd .\content\textures\Cursors\KeyboardMouse # Change to keyboardmouse cursors directory 
+
+Write-Host -ForegroundColor Green "Deleting old cursors"
+Remove-Item -ErrorAction SilentlyContinue -Path .\ArrowCursor.png  # Delete the old mouse curosrs
+Remove-Item -ErrorAction SilentlyContinue -Path .\ArrowFarCursor.png
+Remove-Item -ErrorAction SilentlyContinue -Path .\IBeamCursor.png
+
+Write-Host -ForegroundColor Green "Copying new cursors"
+Copy-Item -Path $resourceDir\data\cursor.png -Destination .\ArrowCursor.png #Copy in new mouse cursors
+Copy-Item -Path $resourceDir\data\cursor.png -Destination .\ArrowFarCursor.png
+Copy-Item -Path $resourceDir\data\cursor.png -Destination .\IBeamCursor.png
+
+Write-Host -ForegroundColor Green "Moving to textures directory"
+cd ..\..\ # Go back to the textures folder
+Write-Host -ForegroundColor Green "Deleting and copying shift lock cursor"
+Remove-Item -ErrorAction SilentlyContinue -Path .\MouseLockedCursor.png # Delete shift lock cursor
+Copy-Item -Path $resourceDir\data\cursor.png -Destination .\MouseLockedCursor.png # Copy new cursor
+
+cd $resourceDir
+Read-Host -Prompt "All done! Press enter to exit the script" 
---
+++ data/FPSUnlockerDownload.ps1
@@ -0,0 +1,36 @@
+﻿<#  EZRoblox
+
+    Copyright (C) 2023 Chacos5
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.#>
+
+$apiUrl = "https://api.github.com/repos/axstin/rbxfpsunlocker/releases/latest" # The url that gets requested
+
+Stop-Process -ErrorAction SilentlyContinue -Name rbxfpsunlocker # Before doing anything kill any current FPS unlocker processes
+Write-Host -ForegroundColor Green "Killed any FPS unlocker processes"
+
+
+$request = Invoke-WebRequest -Uri $apiUrl | ConvertFrom-Json # Make api request, convert to powershell object, store in variable
+Write-Host -ForegroundColor Green "Made API request"
+
+Invoke-WebRequest -Uri $request.assets[0].browser_download_url -OutFile .\temp\unlocker.zip # Invoke a web request to the url housed within the json
+Write-Host -ForegroundColor Green "Downloaded ZIP"
+
+Expand-Archive -Path .\temp\unlocker.zip -DestinationPath .\data -Force # Extract the zip file
+Write-Host -ForegroundColor Green "Extracted ZIP"
+
+Remove-Item -Path .\temp\unlocker.zip # Delete the zip file as it is no longer needed
+
+.\data\rbxfpsunlocker.exe # Run FPS unlocker
+Write-Host -ForegroundColor Green "Ran FPS unlocker. Done!`n`n"
